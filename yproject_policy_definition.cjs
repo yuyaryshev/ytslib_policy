14a081da3c160278d893b025a8673713a67a3fae
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-05-31 20:12:58 +0300

    push_all

--- yproject_policy_definition.cjs
+++ yproject_policy_definition.cjs
@@ -1,5 +1,9 @@
 module.exports.policy = "ytslib_policy";
 module.exports.options = {
- exclude: [] // exclude specified folders and files from projects
+ exclude: [
+  "cjs_require.test.cjs",
+  "mjs_import.test.mjs",
+  "version.js"
+ ] // exclude specified folders and files from projects
  // policy options here
 }
