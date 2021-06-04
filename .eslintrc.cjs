c80369b028d8867cbb9f4bc5c2ff937aa2f0af99
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-06-04 20:26:44 +0300

    1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,17 @@
+const importantOnly = 0;
+const excludeDocs = 1;
+const keepDebug = 1;
+
+const nonImportantWarning = !importantOnly ? 2 : 0;
+const nonImportantError = !importantOnly ? 1 : 0;
+
 module.exports = {
     root: true,
     parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "eslint-plugin-tsdoc", "jsdoc", "eslint-plugin-import", "sonarjs"],
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:sonarjs/recommended", "prettier"],
+    plugins: ["@typescript-eslint", "eslint-plugin-tsdoc", "jsdoc", "eslint-plugin-import", "sonarjs"].filter((item) => typeof item === "string"),
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", !importantOnly && "plugin:sonarjs/recommended", "prettier"].filter(
+        (item) => typeof item === "string",
+    ),
     rules: {
         "no-undef": 0,
         "no-prototype-builtins": 0,
@@ -27,8 +36,8 @@ module.exports = {
         "@typescript-eslint/no-non-null-assertion": 0,
         "@typescript-eslint/no-empty-function": 0,
         "@typescript-eslint/ban-types": 0,
-        "jsdoc/require-jsdoc": 1,
-        "jsdoc/require-description": 1,
+        "jsdoc/require-jsdoc": excludeDocs ? 0 : nonImportantError,
+        "jsdoc/require-description": excludeDocs ? 0 : nonImportantError,
         "tsdoc/syntax": 1,
         // "require-jsdoc": ["error", {
         //     "require": {
@@ -41,5 +50,12 @@ module.exports = {
         // }],
         "sonarjs/no-redundant-jump": 1,
         "sonarjs/cognitive-complexity": 0,
+        "sonarjs/no-redundant-jump": 0,
+        "sonarjs/no-small-switch": 0,
+        "sonarjs/no-unused-collection": nonImportantError,
+        "sonarjs/no-collapsible-if":0,
+        "no-debugger": keepDebug ? 0 : nonImportantError,
+        "@typescript-eslint/no-non-null-asserted-optional-chain": 0,
+        "sonarjs/no-one-iteration-loop": 0,
     },
 };
--- yproject_policy_definition.cjs
+++ yproject_policy_definition.cjs
@@ -3,7 +3,10 @@ module.exports.options = {
  exclude: [
   "cjs_require.test.cjs",
   "mjs_import.test.mjs",
-  "version.js"
+  "version.js",
+  "LICENSE",
+  "README.md",
+  "my_wins.json",
  ] // exclude specified folders and files from projects
  // policy options here
 }
